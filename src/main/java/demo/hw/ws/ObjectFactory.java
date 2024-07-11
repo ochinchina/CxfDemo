e5e2a3078bd1166d0b9fa7ad9e73a9dfa43e44ae
Steven Ou <ochinchina@gmail.com>
2024-07-11 15:33:05 +0800

    Create ObjectFactory.java

---
+++ src/main/java/demo/hw/ws/ObjectFactory.java
@@ -0,0 +1,50 @@
+
+package demo.hw.ws;
+
+import jakarta.annotation.Generated;
+import jakarta.xml.bind.annotation.XmlRegistry;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the demo.hw.ws package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+@Generated(value = "com.sun.tools.xjc.Driver", comments = "JAXB RI v3.0.2", date = "2024-07-11T15:23:34+08:00")
+public class ObjectFactory {
+
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: demo.hw.ws
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link GreetMeSometime }
+     * 
+     */
+    public GreetMeSometime createGreetMeSometime() {
+        return new GreetMeSometime();
+    }
+
+    /**
+     * Create an instance of {@link GreetMeSometimeResponse }
+     * 
+     */
+    public GreetMeSometimeResponse createGreetMeSometimeResponse() {
+        return new GreetMeSometimeResponse();
+    }
+
+}
