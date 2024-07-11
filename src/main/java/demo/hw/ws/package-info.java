da239126a6b0ec794ec40e9bd124f694d7dbf181
Steven Ou <ochinchina@gmail.com>
2024-07-11 15:33:32 +0800

    Create package-info.java

---
+++ src/main/java/demo/hw/ws/package-info.java
@@ -0,0 +1,2 @@
+@jakarta.xml.bind.annotation.XmlSchema(namespace = "http://apache.org/hello_world_async_soap_http/types", elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package demo.hw.ws;
